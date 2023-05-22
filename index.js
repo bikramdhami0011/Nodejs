ddc171c53e27ae7b44a689e9ee917e6abb1e7cc5
Bikram <bikram296@gmail.com>
2023-05-22 12:11:58 +0545

    update

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const {MongoClient}=require('mongodb')
-const url='mongodb://127.0.0.1:27017'
-const client=new MongoClient(url);
-const getdata=async()=>{
-   const con=await client.connect()
-   const dbs=con.db('e-commer')
-   const collect=dbs.collection('product');
-   const result=await collect.find({name:'bikram1'}).toArray();
-   console.log(result)
+const mongoose=require('mongoose');
+const main =async()=>{
+  await mongoose.connect('mongodb://0.0.0.0:27017/e-commer')
+  const data=new mongoose.Schema({
+    name:String,
+    price:Number
+  });
+  const productmodel=mongoose.model('products',data)
+  const insert=new productmodel({name:'biksah',price:20000})
+  const input=await insert.save();
+  console.log(insert);
 }
-getdata()
+const update=async()=>{
+    await mongoose.connect('mongodb://0.0.0.0:27017/e-commer');
+    const prschema=new mongoose.Schema({
+      name:String,
+      price:Number
+    })
+    const prmodel= mongoose.model('products',prschema)
+    const result=await prmodel.updateOne({
+      name:'biksah',
+    },
+    {$set:{price:1900}});
+    console.log(result)
+    // const res=result.update()
+    // console.log(res)
+
+}
+// update()
+const deletecoll=async ()=>{
+  await mongoose.connect('mongodb://0.0.0.0:27017/e-commer')
+  const pschema=new mongoose.Schema({
+    name:String,
+    price:Number,
+  })
+  const pmodel=mongoose.model('products',pschema);
+  const result=await pmodel.deleteOne({name:"biksah"})
+  console.log(result);
+}
+deletecoll();
+// main()
+
